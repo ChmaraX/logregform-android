4f7c5b90016484a641eedb1396e81fe6835397fb
Adam Chmara <adam.chmara1@gmail.com>
2018-04-17 16:39:15 +0200

    Registration activity initialization

--- app/src/main/java/com/example/chmarax/logregform/RegistrationActivity.java
+++ app/src/main/java/com/example/chmarax/logregform/RegistrationActivity.java
@@ -1,13 +1,71 @@
+
 package com.example.chmarax.logregform;
+
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class RegistrationActivity extends AppCompatActivity {
+
+    private ImageView logo, joinus;
+    private AutoCompleteTextView username, email, password;
+    private Button signup;
+    private TextView signin;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        initializeGUI();
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String inputName = username.getText().toString().trim();
+                String inputPw = password.getText().toString().trim();
+                String inputEmail = email.getText().toString().trim();
+
+                if(inputName.isEmpty() || inputPw.isEmpty() || inputEmail.isEmpty()){
+                    Toast.makeText(RegistrationActivity.this,"Please fill all the details.",Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    //////
+                }
+            }
+        });
+
+
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
+
+
+    private void initializeGUI(){
+
+        logo = findViewById(R.id.ivRegLogo);
+        joinus = findViewById(R.id.ivJoinUs);
+        username = findViewById(R.id.atvUsernameReg);
+        email =  findViewById(R.id.atvEmailReg);
+        password = findViewById(R.id.atvPasswordReg);
+        signin = findViewById(R.id.tvSignIn);
+        signup = findViewById(R.id.btnSignUp);
+    }
+
+
+
 }
