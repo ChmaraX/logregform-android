6315ab7fed3362b4baf38e1419edee4ed7c27290
Adam Chmara <adam.chmara1@gmail.com>
2018-04-17 12:12:08 +0200

    Base registration form layout

---
+++ app/src/main/java/com/example/chmarax/logregform/RegistrationActivity.java
@@ -0,0 +1,13 @@
+package com.example.chmarax.logregform;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+}
