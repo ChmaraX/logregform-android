aa66be78df92726f15364853c2a7f86bbe36ae8d
Adam Chmara <adam.chmara1@gmail.com>
2018-04-17 17:04:35 +0200

    Firebase authentication with email and pw

--- app/src/main/java/com/example/chmarax/logregform/RegistrationActivity.java
+++ app/src/main/java/com/example/chmarax/logregform/RegistrationActivity.java
@@ -2,6 +2,8 @@
 package com.example.chmarax.logregform;
 
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,12 +13,18 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class RegistrationActivity extends AppCompatActivity {
 
     private ImageView logo, joinus;
     private AutoCompleteTextView username, email, password;
     private Button signup;
     private TextView signin;
+    private FirebaseAuth firebaseAuth;
 
 
 
@@ -39,7 +47,18 @@ public class RegistrationActivity extends AppCompatActivity {
                     Toast.makeText(RegistrationActivity.this,"Please fill all the details.",Toast.LENGTH_SHORT).show();
                 }
                 else{
-                    //////
+                    firebaseAuth.createUserWithEmailAndPassword(inputEmail,inputPw).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful()){
+                                    Toast.makeText(RegistrationActivity.this,"Registration successful.",Toast.LENGTH_SHORT).show();
+                                    startActivity(new Intent(RegistrationActivity.this,MainActivity.class));
+                                }
+                                else{
+                                    Toast.makeText(RegistrationActivity.this,"Registration failed.",Toast.LENGTH_SHORT).show();
+                                }
+                        }
+                    });
                 }
             }
         });
@@ -64,6 +83,8 @@ public class RegistrationActivity extends AppCompatActivity {
         password = findViewById(R.id.atvPasswordReg);
         signin = findViewById(R.id.tvSignIn);
         signup = findViewById(R.id.btnSignUp);
+
+        firebaseAuth = FirebaseAuth.getInstance();
     }
 
 
