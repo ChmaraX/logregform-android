bf2023318c46f86629e9ea6d56c26f8f1437d15c
Adam Chmara <adam.chmara1@gmail.com>
2018-04-28 17:24:22 +0200

    Splash-screen on start

---
+++ app/src/main/java/com/example/chmarax/logregform/SplashScrActivity.java
@@ -0,0 +1,39 @@
+package com.example.chmarax.logregform;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashScrActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_scr);
+
+        LogoLauncher logoLauncher = new LogoLauncher();
+        logoLauncher.start();
+    }
+
+    private class LogoLauncher extends Thread{
+        public void run(){
+            try{
+                sleep(3000);
+            }catch(InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            startActivity(new Intent(SplashScrActivity.this,RegistrationActivity.class));
+            SplashScrActivity.this.finish();
+
+
+        }
+
+    }
+
+}
