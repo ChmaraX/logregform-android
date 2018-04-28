6a069ec6a258ec21eb779ffd6147a79a1946e95e
Adam Chmara <adam.chmara1@gmail.com>
2018-04-28 19:22:57 +0200

    Splash screen custom animations

--- app/src/main/java/com/example/chmarax/logregform/SplashScrActivity.java
+++ app/src/main/java/com/example/chmarax/logregform/SplashScrActivity.java
@@ -3,10 +3,18 @@ package com.example.chmarax.logregform;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class SplashScrActivity extends AppCompatActivity {
+
+    private ImageView logoSplash, chmaraTech, logoWhite;
+    private Animation anim1, anim2, anim3;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,24 +24,63 @@ public class SplashScrActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash_scr);
 
-        LogoLauncher logoLauncher = new LogoLauncher();
-        logoLauncher.start();
-    }
+        logoSplash = findViewById(R.id.ivLogoSplash);
+        logoWhite = findViewById(R.id.ivLogoWhite);
+        chmaraTech = findViewById(R.id.ivCHTtext);
+        anim1 = AnimationUtils.loadAnimation(getBaseContext(), R.anim.rotate);
+        anim2 = AnimationUtils.loadAnimation(getBaseContext(), R.anim.fadeout);
+        anim3 = AnimationUtils.loadAnimation(getBaseContext(), R.anim.fadein);
 
-    private class LogoLauncher extends Thread{
-        public void run(){
-            try{
-                sleep(3000);
-            }catch(InterruptedException e) {
-                e.printStackTrace();
+        logoSplash.startAnimation(anim1);
+        anim1.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
             }
 
-            startActivity(new Intent(SplashScrActivity.this,RegistrationActivity.class));
-            SplashScrActivity.this.finish();
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                logoSplash.startAnimation(anim2);
+                logoSplash.setVisibility(View.GONE);
+
+                logoWhite.startAnimation(anim3);
+                chmaraTech.startAnimation(anim3);
+                anim3.setAnimationListener(new Animation.AnimationListener() {
+                    @Override
+                    public void onAnimationStart(Animation animation) {
+
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animation animation) {
+                        logoWhite.setVisibility(View.VISIBLE);
+                        chmaraTech.setVisibility(View.VISIBLE);
+
+                        finish();
+                        startActivity(new Intent(SplashScrActivity.this,RegistrationActivity.class));
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animation animation) {
+
+                    }
+                });
 
 
-        }
+
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+
+
 
     }
+
 
 }
