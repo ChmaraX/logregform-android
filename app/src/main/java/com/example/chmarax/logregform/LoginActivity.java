457dbb350d1ad25b6494ff5b84b03bc90df5a8a4
Adam Chmara <adam.chmara1@gmail.com>
2018-04-29 19:42:27 +0200

    Reset password function layout

--- app/src/main/java/com/example/chmarax/logregform/LoginActivity.java
+++ app/src/main/java/com/example/chmarax/logregform/LoginActivity.java
@@ -56,6 +56,13 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        ivSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this,RegistrationActivity.class));
+            }
+        });
+
 
 
     }
